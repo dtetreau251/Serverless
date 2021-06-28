3e2b2cc8345a385b93e126cfbfb835fddb4294a1
David Tetreau <david_tetreau@yahoo.com>
2021-06-27 21:21:59 -0500

    added conditionals to password

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,9 +2,16 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     let password = req.query.password
+    let response
+
+    if(password === "letmein") {
+        response = "Access granted"
+    } else {
+        response = "Access denied"
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: response
     };
 }
