27c487fa74085e3c8c8f6edbd93baa808e6b1402
David Tetreau <david_tetreau@yahoo.com>
2021-07-02 16:17:02 -0500

    stuff

--- song4u/index.js
+++ song4u/index.js
@@ -1,25 +1,24 @@
 const querystring = require('querystring');
 const fetch = require('node-fetch');
 
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
+module.exports = async function (context, req) {	module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');	    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log(req.body)	
     const queryObject = querystring.parse(req.body);
     const url = queryObject.MediaUrl0;
-    // const url = "https://images.unsplash.com/photo-1555888997-03e986fc157b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8b2xkJTIwbWFufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
 
     const binaryData = await downloadImage(url);
     const faceData = await getFaceData(binaryData)
-    // const age = await faceData[0].faceAttributes.age
     const age = faceData[0].faceAttributes.age.toString()
     context.log(age)
 
     const generation = determineAge(age)
     context.log("GENERATION: " + generation)
 
-    context.res = {
+
+    context.res = {	    
         body: `${generation}`
-    };
+    };	   
 }
 
 async function downloadImage(imgUrl) {
@@ -33,8 +32,8 @@ async function downloadImage(imgUrl) {
 
 async function getFaceData(binaryData) {
 
-    const subKey = process.env.SUBSCRIPTIONKEY;
-    const uriBase = process.env.ENDPOINT + 'face/v1.0/detect'
+    const subKey = process.env['SUBSCRIPTIONKEY'];
+    const uriBase = process.env['ENDPOINT'] + 'face/v1.0/detect'
 
     let params = new URLSearchParams({
         'returnFaceId': 'true',
@@ -57,19 +56,20 @@ async function getFaceData(binaryData) {
 }
 
 function determineAge(age) {
-    if (age > 5 && age < 25) {
-        return "GenZ"
-    }
-    else if (age > 24 && age < 41) {
-        return "GenY"
-    }
-    else if (age > 40 && age < 57) {
-        return "GenX"
-    }
-    else if (age > 56 && age < 76) {
-        return "BabyBoomers"
-    }
-    else {
-        return "Unknown"
+        if (age > 5 && age < 25) {
+            return "GenZ"
+        }
+        else if (age > 24 && age < 41) {
+            return "GenY"
+        }
+        else if (age > 40 && age < 57) {
+            return "GenX"
+        }
+        else if (age > 56 && age < 76) {
+            return "BabyBoomers"
+        }
+        else {
+            return "Unknown"
+        }
     }
 }
