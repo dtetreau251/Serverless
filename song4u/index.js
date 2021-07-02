1472623ad3a28b7a0b27b8874728160c25ce23a8
David Tetreau <david_tetreau@yahoo.com>
2021-07-02 15:30:31 -0500

    fixed a curly brace

--- song4u/index.js
+++ song4u/index.js
@@ -1,13 +1,16 @@
-const querystring = require('querystring')
-const fetch = require('node-fetch')
+const querystring = require('querystring');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    context.log(req.body)
+
     const queryObject = querystring.parse(req.body);
     const url = queryObject.MediaUrl0;
+    // const url = "https://images.unsplash.com/photo-1555888997-03e986fc157b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8b2xkJTIwbWFufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+
     const binaryData = await downloadImage(url);
     const faceData = await getFaceData(binaryData)
+    // const age = await faceData[0].faceAttributes.age
     const age = faceData[0].faceAttributes.age.toString()
     context.log(age)
 
@@ -30,8 +33,8 @@ async function downloadImage(imgUrl) {
 
 async function getFaceData(binaryData) {
 
-    const subKey = process.env['SUBSCRIPTIONKEY'];
-    const uriBase = process.env['ENDPOINT'] + 'face/v1.0/detect'
+    const subKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + 'face/v1.0/detect'
 
     let params = new URLSearchParams({
         'returnFaceId': 'true',
