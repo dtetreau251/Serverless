f1ac0c496b242d58930b1559158e6e976e8fed59
David Tetreau <david_tetreau@yahoo.com>
2021-07-02 17:34:49 -0500

    stuff

--- song4u/index.js
+++ song4u/index.js
@@ -1,23 +1,23 @@
-const querystring = require('querystring');
-const fetch = require('node-fetch');
+let querystring = require('querystring');
+let fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');	
     context.log(req.body)	
-    const queryObject = querystring.parse(req.body);
-    const url = queryObject.MediaUrl0;
+    let queryObject = querystring.parse(req.body);
+    let url = queryObject.MediaUrl0;
 
-    const binaryData = await downloadImage(url);
-    const faceData = await getFaceData(binaryData)
-    const age = faceData[0].faceAttributes.age.toString()
+    let binaryData = await downloadImage(url);
+    let faceData = await getFaceData(binaryData)
+    let age = faceData[0].faceAttributes.age.toString()
     context.log(age)
 
-    const generation = determineAge(age)
+    let generation = determineAge(age)
     context.log("GENERATION: " + generation)
 
 
     context.res = {	    
-        body: `${generation}`
+        body: generation
     };	   
 }
 
@@ -32,15 +32,15 @@ async function downloadImage(imgUrl) {
 
 async function getFaceData(binaryData) {
 
-    const subKey = process.env['SUBSCRIPTION_KEY'];
-    const uriBase = process.env['API_ENDPOINT'] + 'face/v1.0/detect'
+    let subKey = process.env['SUBSCRIPTIONKEY'];
+    let uriBase = process.env['ENDPOINT'] + 'face/v1.0/detect'
 
     let params = new URLSearchParams({
         'returnFaceId': 'true',
         'returnFaceAttributes': 'age' 
     });
 
-    const urlToUse = uriBase + '?' + params.toString()
+    let urlToUse = uriBase + '?' + params.toString()
     // making the post request
     let resp = await fetch(urlToUse,{
         method: 'POST',
