7dd6a06dd3806b3e97815ad2e0977e80dbe0d9fb
David Tetreau <david_tetreau@yahoo.com>
2021-07-02 14:35:09 -0500

    fixed a typo

--- song4u/index.js
+++ song4u/index.js
@@ -3,7 +3,7 @@ const querystring = require('querystring')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     var reqbody = req.body
-    context.log(reqbody)
+    context.log(req.body)
     const queryObject = querystring.parse(req.body);
 
     context.res = {
