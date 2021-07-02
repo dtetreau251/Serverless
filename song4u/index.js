d165bf0727a21fbc86b048174d22b7e8c237f140
David Tetreau <david_tetreau@yahoo.com>
2021-07-02 14:57:42 -0500

    step 7

--- song4u/index.js
+++ song4u/index.js
@@ -1,13 +1,72 @@
 const querystring = require('querystring')
+const fetch = require('node-fetch')
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    var reqbody = req.body
     context.log(req.body)
     const queryObject = querystring.parse(req.body);
 
+    const binaryData = await downloadImage(url);
+    const faceData = await getFaceData(binaryData)
+    const age = faceData[0].faceAttributes.age.toString()
+    context.log(age)
+
+    const generation = determineAge(age)
+    context.log("GENERATION: " + generation)
+
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: queryObject.MediaUrl0
+        body: `${generation}`
     };
+}
+
+async function downloadImage(imgUrl) {
+    let resp = await fetch(imgUrl,{
+        method: 'GET',
+    })
+
+    let data = await resp.arrayBuffer();
+    return data;
+}
+
+async function getFaceData(binaryData) {
+
+    const subKey = process.env['SUBKEY'];
+    const uriBase = process.env['ENDPOINT'] + 'face/v1.0/detect'
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'age' 
+    });
+
+    const urlToUse = uriBase + '?' + params.toString()
+    // making the post request
+    let resp = await fetch(urlToUse,{
+        method: 'POST',
+        body: binaryData,
+        headers: {
+            'Content-Type' : 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subKey
+        }
+    })
+    // receive the response
+    let data = await resp.json();
+    return data;
+}
+
+function determineAge(age) {
+    if (age > 5 && age < 25) {
+        return "GenZ"
+    }
+    else if (age > 24 && age < 41) {
+        return "GenY"
+    }
+    else if (age > 40 && age < 57) {
+        return "GenX"
+    }
+    else if (age > 56 && age < 76) {
+        return "BabyBoomers"
+    }
+    else {
+        return "Unknown"
+    }
 }
