65d8286e1902725ded7f7c00c2a647bfc5b505fe
David Tetreau <david_tetreau@yahoo.com>
2021-07-02 20:38:51 -0500

    added songs

--- song4u/index.js
+++ song4u/index.js
@@ -1,4 +1,4 @@
-const querystring = require('query-string');
+const querystring = require('querystring');
 const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
@@ -12,10 +12,11 @@ module.exports = async function (context, req) {
     context.log(age)
 
     const generation = determineAge(age)
+    const song = getSong(generation)
     context.log("GENERATION: " + generation)
 
     context.res = {
-        body: generation
+        body: `We guessed you're part of this generation: ${generation}! Happy listening! ${song}`
     };
 }
 
@@ -53,7 +54,8 @@ async function getFaceData(binaryData) {
     return data;
 }
 
-function determineAge(age) {
+function determineAge(ageString) {
+    let age = parseInt(ageString);
     if (age > 5 && age < 25) {
         return "GenZ"
     }
@@ -69,4 +71,14 @@ function determineAge(age) {
     else {
         return "Unknown"
     }
+}
+
+function getSong(gen) {
+    const songs = {"GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+    "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+    "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+    "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+    "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
+
+    return songs[gen]
 }
