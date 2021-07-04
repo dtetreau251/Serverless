c48870fa19a32a9f9f2b5f4487f6068294475e84
David Tetreau <david_tetreau@yahoo.com>
2021-07-04 15:40:58 -0500

    if else

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -8,20 +8,19 @@ module.exports = async function (context, req) {
 
     var boundary = multipart.getBoundary(req.headers['content-type']);
     var body = req.body;
-    var headers = req.headers;
     var responseMessage = ""
     if (body == null) {
         responseMessage = "Sorry! No image attached."
-    } else {
-        var password =  req.headers['codename'];
+    } 
+    else {
         var parsedBody = multipart.Parse(body, boundary);
         // get the file extension
         var ext = determineExt(parsedBody[0].type);
+        var password = headers.codename;
         // upload the file to blob
         responseMessage = await uploadFile(parsedBody, ext, password);
     }
 
-    var parsedBody = multipart.Parse(body, boundary);
     var filetype = parsedBody[0].type;
     if (filetype == "image/png") {
         ext = "png";
