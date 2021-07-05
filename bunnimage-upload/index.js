71d64aaed1aaac8eb4697c4b03322d8494369227
David Tetreau <david_tetreau@yahoo.com>
2021-07-05 11:27:26 -0500

    week 3 video help

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -6,25 +6,34 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.log(connectionString)
 
-    var boundary = multipart.getBoundary(req.headers['content-type']);
-    var body = req.body;
     var responseMessage = "";
-    var headers = req.headers;
-     
-    if (body == null) {
-        responseMessage = "Sorry! No image attached."
-    } else {
-        var password = headers['codename'];
+    try {
+        var password = req.headers['codename'];
+        var boundary = multipart.getBoundary(req.headers['content-type']);
+        var body = req.body;
         var parsedBody = multipart.Parse(body, boundary);
-        // get the file extension
-        var ext = determineExt(parsedBody[0].type);
-        // upload the file to blob
+        var filetype = parsedBody[0].type;
+        if (filetype == "image/png") {
+            ext = "png";
+        } else if (filetype == "image/jpeg") {
+            ext = "jpeg";
+        } else if (filetype == "image/jpg") {
+            ext = "jpg"
+        } else {
+            username = "invalidimage"
+            ext = "";
+        }
+        
         responseMessage = await uploadFile(parsedBody, ext, password);
+        
+    } catch(err) {
+        context.log("Undefined bodu image");
+        responseMessage = "Sorry! No image attached.";
     }
 
     context.res = {
         body: responseMessage
-    }
+    }; 
 }
 
 async function uploadFile(parsedBody, ext, password) {
@@ -35,18 +44,4 @@ async function uploadFile(parsedBody, ext, password) {
     const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
     const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
     return uploadBlobResponse;
-}
-
-function determineExt(filetype) {
-    if (filetype == "image/png") {
-        ext = "png";
-    } else if (filetype == "image/jpeg") {
-        ext = "jpeg";
-    } else if (filetype == "image/jpg") {
-        ext = "jpg"
-    } else {
-        username = "invalidimage"
-        ext = "";
-    }
-    return ext
 }
