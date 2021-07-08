b4c799cc861178130c3eac1af76816d953297c0e
David Tetreau <david_tetreau@yahoo.com>
2021-07-08 14:38:47 -0500

    url instead of uri

--- bunnimage-download/index.js
+++ bunnimage-download/index.js
@@ -50,7 +50,7 @@ module.exports = async function (context, req) {
     context.res = {
         // status: 200, /* Defaults to 200 */
             body: {
-                "downloadUri": download,
+                "downloadUrl": download,
                 "success": success
             }
     };
