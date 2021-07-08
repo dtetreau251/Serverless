43b98c0e0391e59b9645648b9412526d4eb64252
David Tetreau <david_tetreau@yahoo.com>
2021-07-07 20:54:59 -0500

    bunnimage

--- bunnimage-download/index.js
+++ bunnimage-download/index.js
@@ -3,7 +3,7 @@ var fetch = require("node-fetch");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let blobname = "images";
+    let blobname = "bunnimage";
     var username = req.headers['username'];
     var download = ""
     var downloadpng = "https://" + blobname + ".blob.core.windows.net/images/" + username + ".png";
