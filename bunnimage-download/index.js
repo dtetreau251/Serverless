6b840fd222fd7e73b8b1e26b9a8a206a3394b30a
David Tetreau <david_tetreau@yahoo.com>
2021-07-08 00:05:40 -0500

    body changed

--- bunnimage-download/index.js
+++ bunnimage-download/index.js
@@ -49,11 +49,11 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        "body": {
             body: {
                 "downloadUri": download,
                 "success": success
             }
-        }
     };
+    context.log(download);
+    context.done();
 }
