5a63e91979c5c0c8ad4f398f2acd1e48dae2eb37
David Tetreau <david_tetreau@yahoo.com>
2021-07-08 15:11:52 -0500

    uri

--- bunnimage-download/index.js
+++ bunnimage-download/index.js
@@ -50,7 +50,7 @@ module.exports = async function (context, req) {
     context.res = {
         // status: 200, /* Defaults to 200 */
             body: {
-                "downloadUrl": download,
+                "downloadUri": download,
                 "success": success
             }
     };
