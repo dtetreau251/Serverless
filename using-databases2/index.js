8651ac7f576c25de581bb221c24daa5998d14543
David Tetreau <david_tetreau@yahoo.com>
2021-07-10 10:43:48 -0500

    changes

--- using-databases2/index.js
+++ using-databases2/index.js
@@ -1,62 +1,35 @@
-var multipart = require("parse-multipart");
 const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
 const { BlobServiceClient } = require("@azure/storage-blob");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-module.exports = async function (context, myTimer, req) {
-    var timeStamp = new Date().toISOString();
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString(); 
+    await fetch('https://cataas.com/cat')
+        .then(async function(response) {
+            context.log(response)
+            let buffer = await response.arrayBuffer()
+            context.log(data);
+            let base64data = Buffer.from(buffer).toString('base64');
+            //context.log(base64data);
+            context.log('JavaScript HTTP trigger function processed a request.');
+            return base64data;
+        })
+        .then(async function(data) {
+            const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+            context.log(blobServiceClient);
+            const containerName = "images"
+            //context.log('\nCreating container...');
+            //context.log('\t', containerName);
+            const containerClient = blobServiceClient.getContainerClient(containerName);
+            //context.log(containerClient);
+            const blobName = 'cat' + '.' + 'jpeg';
+            const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+            //context.log('\nUploading to Azure storage as blob:\n\t', blobName);
+            const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+            context.log("Blob was uploaded successfully. requestId: ", uploadBlobResponse.requestId);
+            return "File Saved";    
+        })
 
-    let resp = await fetch("https://cataas.com/cat/cute/says/Bitcamp", {
-        method: 'GET',
-        headers: {"Content-type": "application/octet-stream"}
-    });
-    
-    let boundary = {"Content-type": "application/octet-stream"}
-    let data = await resp.arrayBuffer()
-    context.log(data)
-    data = Buffer.from(data).toString('base64')
- 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: {
-            cat: data
-        }
-    };
-
-    context.log('JavaScript HTTP trigger function processed a request.');
-    var body = data;
-    var parsedBody = multipart.Parse(body, boundary);
-    context.log(parsedBody);
-    var fileExtension = parsedBody.type;
-    //fileExtension = fileName.replace(/^.*\./, '');
-    if (fileExtension == "image/png") {
-        ext = "png";
-    } else if (fileExtension == "image/jpeg") {
-        ext = "jpeg";
-    } else {
-        username = "invalidimage"
-        ext = "";
-    }
-
-    var responseMessage = await uploadFile(parsedBody, ext);
-    context.res = {
-        body: responseMessage
-    };
-    console.log(responseMessage)
-
-    async function uploadFile(parsedBody, ext){
-        const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
-        const containerName = "images"
-        console.log('\nCreating container...');
-        console.log('\t', containerName);
-        const containerClient = blobServiceClient.getContainerClient(containerName);
-        const blobName = 'test' + "." + ext;
-        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-        console.log('\nUploading to Azure storage as blob:\n\t', blobName);
-        const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
-        console.log("Blob was uploaded successfully. requestId: ", uploadBlobResponse.requestId);
-        return "File Saved";    
-}
 
     if (myTimer.isPastDue)
     {
