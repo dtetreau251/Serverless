6cbfb58265a3ea9719ef4425f4605b3c485d467d
David Tetreau <david_tetreau@yahoo.com>
2021-07-10 22:04:11 -0500

    modified function

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -2,9 +2,14 @@ const querystring = require('querystring')
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+
     const queryObject = querystring.parse(req.body);
+    message = queryObject.Body;
+
+    const responseMessage = message
 
     context.res = {
-        body: queryObject.Body
-     };     
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };    
 }
