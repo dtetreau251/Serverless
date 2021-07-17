19cbab96fd131c3258a417eb28cb753f2aa014d8
David Tetreau <david_tetreau@yahoo.com>
2021-07-17 09:23:34 -0500

    passed in name and changed function name

--- twocatz/index.js
+++ twocatz/index.js
@@ -8,7 +8,7 @@ module.exports = async function (context, req) {
     let name3 = req.query.name3
     let name4 = req.query.name4
 
-    async function getCat() {
+    async function getCatPic(name) {
         let resp = await fetch("https://cataas.com/cat/says/Serverless", { method: "GET" });
         let data = await resp.arrayBuffer();
 
@@ -16,10 +16,10 @@ module.exports = async function (context, req) {
         return base64data
     }
 
-    let firstcat = await getCat(name1)
-    let secondcat = await getCat(name2)
-    let thirdcat = await getCat(name3)
-    let fourthcat = await getCat(name4)
+    let firstcat = await getCatPic(name1)
+    let secondcat = await getCatPic(name2)
+    let thirdcat = await getCatPic(name3)
+    let fourthcat = await getCatPic(name4)
 
     context.res = {
         // status: 200, /* Defaults to 200 */
