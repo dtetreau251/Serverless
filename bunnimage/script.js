87d415739dbefeb81da6cedbb27f4135fe794f9d
David Tetreau <david_tetreau@yahoo.com>
2021-07-16 23:58:34 -0500

    finished step 3

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,7 +1,33 @@
+
 function getImage(event) {
     event.preventDefault()
+    const myform = document.getElementById("myform");
+    const nameInput = document.getElementById("name");
+    const fileInput = document.getElementById("image");
+    const file = fileInput.files[0];
+
+    const payload = new FormData(myform);
+    console.log(payload);
+    payload.append("file", file);
+
     if (document.getElementById('name').value != '') {
-        $('#output').text("Thanks!")
+
+        try {
+            const url = process.env.BUNNIMAGE_ENDPOINT;
+            console.log("Image was uploaded, making POST request to Azure function")
+
+            const resp = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'codename': nameInput.value
+                },
+                body: payload
+            })
+            $('#output').text("Your image has been stored successfully!")
+        } catch(err) {
+            $('#output').text(err)
+        }
+
     } else {
         alert("No name error.")
     }
