3d0a653ae244a656d3596ded8c2c65532bded437
David Tetreau <david_tetreau@yahoo.com>
2021-07-17 20:50:44 -0500

    refactored

--- catzapp/script.js
+++ catzapp/script.js
@@ -1,15 +1,15 @@
  async function y1k3s() {
-
     let url = `https://twocatz3.azurewebsites.net/api/twocatz?name1=${document.getElementById("name1").value}&name2=${document.getElementById("name2").value}&name3=${document.getElementById("name3").value}&name4=${document.getElementById("name4").value}`
-    let resp = await fetch(url, {
-        method: 'GET',
-        mode: 'no-cors'
+    console.log(url)
+   let response = await fetch(url, {
+        method: 'GET', 
     });
- 
-    let result = await resp.json()
 
-    document.getElementById("image1").src = "data:image/png;base64," + result.cat1
-    document.getElementById("image2").src = "data:image/png;base64," + result.cat2
-    document.getElementById("image3").src = "data:image/png;base64," + result.cat3
-    document.getElementById("image4").src = "data:image/png;base64," + result.cat4
-  }
+    let data = await response.json();
+
+      document.getElementById("image1").src = "data:image/png;base64," + data.cat1
+      document.getElementById("image2").src = "data:image/png;base64," + data.cat2
+      document.getElementById("image3").src = "data:image/png;base64," + data.cat3
+      document.getElementById("image4").src = "data:image/png;base64," + data.cat4
+}
+    
